cd24c4ed8435ad7747e94265af9af9a6a05d202e
brianonchieku <brianonchikeku0@gmail.com>
2025-01-29 20:45:34 +0300

    signup

--- app/src/main/java/com/example/authrealm/view/LoginPage.kt
+++ app/src/main/java/com/example/authrealm/view/LoginPage.kt
@@ -7,18 +7,20 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -46,43 +48,45 @@ import com.example.authrealm.R
 import com.example.authrealm.UserViewmodel
 
 @Composable
-fun SignupPage(viewmodel: UserViewmodel){
-
+fun LoginPage(viewmodel: UserViewmodel){
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Image(painter = painterResource(id = R.drawable.lavender3), contentDescription =null,
             modifier = Modifier.fillMaxSize(), contentScale = ContentScale.Crop)
         Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-
-            var username by remember { mutableStateOf("") }
-            var password by remember { mutableStateOf("") }
-            var confirmpassword by remember { mutableStateOf("") }
-            val signupStatus by viewmodel.signupStatus.collectAsState()
-            val context = LocalContext.current
-
-            LaunchedEffect(signupStatus) {
-                when (signupStatus) {
-                    is NetworkResponse.Error -> {
-                        Toast.makeText(context, (signupStatus as NetworkResponse.Error).message, Toast.LENGTH_SHORT).show()
-                    }
-                    is NetworkResponse.Success -> {
-                        Toast.makeText(context, (signupStatus as NetworkResponse.Success).message, Toast.LENGTH_SHORT).show()
-                    }
-                    else -> {} // No toast for loading state
-                }
-            }
             Card(modifier = Modifier
                 .wrapContentSize()
-                .verticalScroll(rememberScrollState())
-                .padding(30.dp)) {
+                .padding(30.dp),
+                shape = RoundedCornerShape(20.dp)
+            ) {
+                var userName by remember { mutableStateOf("") }
+                var password by remember { mutableStateOf("") }
+                var checkText by remember{ mutableStateOf(false) }
+                val loginStatus by viewmodel.loginStatus.collectAsState()
+                val context = LocalContext.current
+
+                LaunchedEffect(loginStatus) {
+                    when(loginStatus){
+                        is NetworkResponse.Error -> {
+                            Toast.makeText(context, (loginStatus as NetworkResponse.Error).message, Toast.LENGTH_SHORT).show()
+                        }
+                        NetworkResponse.Loading -> {}
+                        is NetworkResponse.Success -> {
+                            Toast.makeText(context, (loginStatus as NetworkResponse.Success).message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+
                 Column(modifier = Modifier.wrapContentSize(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Spacer(modifier = Modifier.size(20.dp))
-                    Text(text = "Register", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    Text(text = "Login", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                     Spacer(modifier = Modifier.size(20.dp))
-                    OutlinedTextField(value =username , onValueChange ={username=it},
-                        label = { Text(text = "Username") },
-                        placeholder = { Text(text = "Enter Username") },
+                    OutlinedTextField(value =userName , onValueChange ={userName=it},
+                        label = { Text(text = "username") },
+                        placeholder = { Text(text = "Enter username") },
                         leadingIcon = {
-                            Icon(painterResource(id = R.drawable.baseline_person_24), contentDescription =null )
+                            Icon(painterResource(id = R.drawable.baseline_email_24), contentDescription =null )
                         }, shape = RoundedCornerShape(20.dp)
                     )
                     Spacer(modifier = Modifier.size(20.dp))
@@ -91,36 +95,44 @@ fun SignupPage(viewmodel: UserViewmodel){
                         placeholder = { Text(text = "Enter password") },
                         leadingIcon = {
                             Icon(painterResource(id = R.drawable.baseline_lock_24), contentDescription =null )
-                        }, shape = RoundedCornerShape(20.dp),
-                        visualTransformation = PasswordVisualTransformation()
+                        }, shape = RoundedCornerShape(20.dp)
+                        , visualTransformation = PasswordVisualTransformation()
                     )
                     Spacer(modifier = Modifier.size(20.dp))
-                    OutlinedTextField(value =confirmpassword , onValueChange ={confirmpassword=it},
-                        label = { Text(text = "confirm password") },
-                        placeholder = { Text(text = "confirm password") },
-                        leadingIcon = {
-                            Icon(painterResource(id = R.drawable.baseline_lock_24), contentDescription =null )
-                        }, shape = RoundedCornerShape(20.dp),
-                        visualTransformation = PasswordVisualTransformation()
-                    )
-                    Spacer(modifier = Modifier.size(20.dp))
+                    Row(modifier = Modifier.wrapContentWidth(), verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(checked = checkText, onCheckedChange ={checkText=it} )
+                        Text(text = "Remember me?")
 
+                    }
+                    Spacer(modifier = Modifier.size(20.dp))
                     Button(onClick ={
-                        viewmodel.signup(username, password)
-                    }, modifier = Modifier.width(250.dp), ) {
-                        when(signupStatus){
-
-                            is NetworkResponse.Loading -> {
+                        if (userName.isNotEmpty() || password.isNotEmpty()) {
+                            viewmodel.login(userName, password)
+                        } else {
+                            Toast.makeText(context, "Please fill all fields", Toast.LENGTH_SHORT).show()
+                        }
+                    }, modifier = Modifier.width(250.dp) ) {
+                        when(loginStatus){
+                            NetworkResponse.Loading -> {
                                 CircularProgressIndicator(color = Color.White, modifier = Modifier.size(24.dp))
                             }
-                            else -> {
-                                Text(text = "Register", fontWeight = FontWeight.Bold)
+                            else ->{
+                                Text(text = "Login", fontWeight = FontWeight.Bold)
                             }
+
                         }
                     }
                     Spacer(modifier = Modifier.size(20.dp))
-                    Text(text = "Already have an account?", fontWeight = FontWeight.Bold,
-                        modifier = Modifier.clickable {})
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(30.dp),
+                        verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(text = "forgot password?", modifier = Modifier.clickable {})
+                        Text(text = "Register", fontWeight = FontWeight.Bold, modifier = Modifier.clickable {})
+
+                    }
 
 
 
@@ -134,5 +146,4 @@ fun SignupPage(viewmodel: UserViewmodel){
 
 
     }
-
 }
