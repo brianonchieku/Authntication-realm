88e2082c63c07ab7eaba5fe7a6826a55a87a2cb7
brianonchieku <brianonchikeku0@gmail.com>
2025-01-29 12:30:42 +0300

    user model

---
+++ app/src/main/java/com/example/authrealm/models/UserModel.kt
@@ -0,0 +1,12 @@
+package com.example.authrealm.models
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+class UserModel: RealmObject {
+
+    @PrimaryKey val id: ObjectId = ObjectId()
+    var userName: String = ""
+    var password: String = ""
+}
